1647d1bb123d60f8a55545b91d3a603b80b66d68
elbaltezar <norberto.baltezar@gmail.com>
2025-01-09 18:39:51 -0600

    Update * Fila de totales agregadas a las tablas de 'Gastos'. * Se modificaron algunos los métodos de 'helpers.ts', para volverlos asíncronos y se pudieran calcular los totales. * Se modificaron algunos métodos de vistas del módulo de PCS, para volverlos asíncronos. * Se modificaron algunos Pipes del módulo de PCS.

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,13 +1,13 @@
 
 import * as base64js from 'base64-js'
-import { addMonths, differenceInCalendarMonths, format} from 'date-fns';
+import { addMonths, differenceInCalendarMonths, format } from 'date-fns';
 import { es } from 'date-fns/locale';
 import { SeccionData, SeccionFormateada, SeccionSubseccion } from 'src/app/pcs/models/pcs.model';
 import { Mes } from 'src/models/general.model';
 
 
 
-export const descargarArchivo = async (base64Datos: string, nombreArchivo: string): Promise<void>  => {
+export const descargarArchivo = async (base64Datos: string, nombreArchivo: string): Promise<void> => {
   const base64String = base64Datos.split(';base64,').pop()
   const byteArray = base64js.toByteArray(base64String);
   const blob = new Blob([byteArray], { type: 'application/octet-stream' });
@@ -27,27 +27,27 @@ export const descargarArchivo = async (base64Datos: string, nombreArchivo: strin
 }
 
 const mimeTypeExtensions: any = {
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':        'xlsx',
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.document':  'docx',
-  'application/pdf':  'pdf',
-  'image/png':        'png',
-  'image/jpg':        'jpg',
-  'image/jpeg':       'jpeg'
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
+  'application/pdf': 'pdf',
+  'image/png': 'png',
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpeg'
 };
 
-export const obtenerMeses = (fechaInicio: Date, fechaFin: Date): Mes[] => {
+export const obtenerMeses = async (fechaInicio: Date, fechaFin: Date): Promise<Mes[]> => {
 
   const diferenciaMeses = differenceInCalendarMonths(fechaFin, fechaInicio);
   const meses: Mes[] = []
-  
+
   for (let i = 0; i <= diferenciaMeses; i++) {
     const fecha = addMonths(fechaInicio, i)
-    const mes   = +format(fecha, 'M')
-    const anio  = +format(fecha, 'Y')
-    const desc  = format(fecha, 'LLL/Y', {locale: es})
+    const mes = +format(fecha, 'M')
+    const anio = +format(fecha, 'Y')
+    const desc = format(fecha, 'LLL/Y', { locale: es })
 
     meses.push({
-      mes:  mes,
+      mes: mes,
       anio: anio,
       desc: desc
     })
@@ -56,39 +56,34 @@ export const obtenerMeses = (fechaInicio: Date, fechaFin: Date): Mes[] => {
   return meses
 }
 
-export const obtenerMesesTrans = (fechaInicio: Date, fechaFin: Date):number => {
+export const obtenerMesesTrans = (fechaInicio: Date, fechaFin: Date): number => {
 
-  
- 
   const diferenciaMeses = differenceInCalendarMonths(fechaFin, fechaInicio);
-          console.log("diferenciaMeses: " + diferenciaMeses)
+  console.log("diferenciaMeses: " + diferenciaMeses)
 
-        
-          
-          for (let i = 0; i <= diferenciaMeses; i++) {
-            const fecha = addMonths(fechaInicio, i)
-            const mes   = +format(fecha, 'M')
-            const anio  = +format(fecha, 'Y')
-     
-           
-          }
+
+  for (let i = 0; i <= diferenciaMeses; i++) {
+    const fecha = addMonths(fechaInicio, i)
+    const mes = +format(fecha, 'M')
+    const anio = +format(fecha, 'Y')
+  }
 
   return diferenciaMeses
 }
 
-export const obtenerMesesFecha = (fechaInicio: Date, fechaFin: Date): Mes[] => {
+export const obtenerMesesFecha = async (fechaInicio: Date, fechaFin: Date): Promise<Mes[]> => {
 
   const diferenciaMeses = differenceInCalendarMonths(fechaFin, fechaInicio);
   const meses: Mes[] = []
-  
+
   for (let i = 0; i <= diferenciaMeses; i++) {
     const fecha = addMonths(fechaInicio, i)
-    const mes   = +format(fecha, 'M')
-    const anio  = +format(fecha, 'Y')
-    const desc  = format(fecha, 'LLL/Y', {locale: es})
+    const mes = +format(fecha, 'M')
+    const anio = +format(fecha, 'Y')
+    const desc = format(fecha, 'LLL/Y', { locale: es })
 
     meses.push({
-      mes:  mes,
+      mes: mes,
       anio: anio,
       desc: desc
     })
@@ -108,8 +103,8 @@ export const formatCurrency = (valor: number) => {
 /**
  * Función para dar formato a las tablas de PCS Control
  */
-export const formatearInformacionControl = (data: SeccionData | SeccionSubseccion): SeccionFormateada | null => {
-  
+export const formatearInformacionControl = async (data: SeccionData | SeccionSubseccion): Promise<SeccionFormateada | null> => {
+
   // Paso 1: Unir y ordenar los meses y años de ambos arreglos
   const fechasCombinadas = [...data.previsto.fechas, ...data.real.fechas]
     .map(f => ({ mes: f.mes, anio: f.anio }))
@@ -117,7 +112,7 @@ export const formatearInformacionControl = (data: SeccionData | SeccionSubseccio
       (v, i, a) => a.findIndex(t => t.mes === v.mes && t.anio === v.anio) === i
     ) // Eliminar duplicados
     .sort((a, b) => a.anio - b.anio || a.mes - b.mes); // Ordenar por año y luego por mes;
-  
+
   // Paso 2: Crear los nombres de los meses en formato 'ENE/2024'
   let encabezados = fechasCombinadas.map(f => {
     const mesNombres = ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC'];
@@ -125,7 +120,7 @@ export const formatearInformacionControl = (data: SeccionData | SeccionSubseccio
   });
 
   encabezados = ['Nombre', 'Subtotal', ...encabezados];
-  
+
   // Paso 3: Crear los valores correspondientes para "previsto" y "real"
   let registros = [];
 
@@ -135,19 +130,19 @@ export const formatearInformacionControl = (data: SeccionData | SeccionSubseccio
   });
 
   registros.push(['Previsto', data.previsto.subTotal || 0, ...valoresPrevisto]);
-  
+
   const valoresReal = fechasCombinadas.map(f => {
     const entry = data.real.fechas.find(e => e.mes === f.mes && e.anio === f.anio);
     return entry ? entry.porcentaje : 0;
   });
-  
+
   registros.push(['REAL', data.real.subTotal || 0, ...valoresReal]);
-  
+
   return {
     hasChildren: data?.hasChildren || false,
     seccion: data?.seccion,
-    encabezados, 
+    encabezados,
     registros,
   };
-  
+
 }
